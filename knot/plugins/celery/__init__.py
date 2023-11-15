927b126f3f717b25f1c3474335d4adba049f2758
Retr0327 <retr0327.dev@gmail.com>
2023-11-16 00:49:54 +0800

    Remove message bus container

--- knot/plugins/celery/__init__.py
+++ knot/plugins/celery/__init__.py
@@ -1,5 +1,4 @@
 from .bus import MessageBus
-from .container import MessageBusContainer
 from .queue import queue_dispatcher_module
 
-__all__ = ["MessageBus", "MessageBusContainer", "queue_dispatcher_module"]
+__all__ = ["MessageBus", "queue_dispatcher_module"]
