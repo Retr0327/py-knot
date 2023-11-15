46ec434574b882fada3048b5787398d66d9a4da3
Retr0327 <retr0327.dev@gmail.com>
2023-11-15 22:14:48 +0800

    Export mock entities

---
+++ tests/mocks/__init__.py
@@ -0,0 +1,15 @@
+from .entities import (
+    MockCommand,
+    MockCommandHandler,
+    MockQuery,
+    MockQueryHandler,
+    MockReturnType,
+)
+
+__all__ = [
+    "MockCommand",
+    "MockCommandHandler",
+    "MockQuery",
+    "MockQueryHandler",
+    "MockReturnType",
+]
