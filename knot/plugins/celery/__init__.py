ce195a0eaf4bc8f72b9cf1aebb094a8837d32951
Retr0327 <retr0327.dev@gmail.com>
2023-11-15 22:12:03 +0800

    Export message bus container

---
+++ knot/plugins/celery/__init__.py
@@ -0,0 +1,5 @@
+from .bus import MessageBus
+from .container import MessageBusContainer
+from .queue import queue_dispatcher_module
+
+__all__ = ["MessageBus", "MessageBusContainer", "queue_dispatcher_module"]
